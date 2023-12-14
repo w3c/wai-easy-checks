852a65d1db130cf3805c8bb9eae7289bc899d52f
Kevin White <kevin@w3.org>
2023-12-14 17:30:26 +0000

    Fix #215

--- bookmarklets/headings.js
+++ bookmarklets/headings.js
@@ -28,7 +28,6 @@ function render() {
   return instance;
 }
 function check(e, key) {
-	console.log("function: check");
   var tasks = 0;
   var i = 0;
   for (; i < e.length; i++) {
@@ -39,7 +38,6 @@ function check(e, key) {
   return tasks;
 }
 function parse(options) {
-	console.log("function: parse");
   var output = "";
   var i = 0;
   for (; i < options.length; i++) {
@@ -65,7 +63,6 @@ function htmlEntities(s) {
   return String(s).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
 }
 function isVisible(elem) {
-	console.log("function: isVisible");
   var style = window.getComputedStyle(elem);
   var i = false;
   for (; elem;) {
@@ -104,7 +101,6 @@ function callback(node) {
   }
 }
 function init(element, animate) {
-	console.log("function: init");
   if (!animate) {
     if (element.scrollIntoViewIfNeeded) {
       element.scrollIntoViewIfNeeded();
@@ -144,7 +140,6 @@ function init(element, animate) {
   }, 100);
 }
 function register(t) {
-	console.log("function: register");
   var init = function() {
     if (t.checked) {
       initFileListMenu();
@@ -156,7 +151,6 @@ function register(t) {
   t.addEventListener("click", init);
 }
 function hide(time) {
-	console.log("function: hide");
   var args = doc.querySelectorAll("#headings a");
   var i = args.length - 1;
   for (; i >= 0; i--) {
@@ -168,7 +162,6 @@ function hide(time) {
   }
 }
 function remove(evt) {
-	console.log("function: remove");
   var liTag = evt.target;
   var parentNode = document.body.querySelectorAll("*");
   var o = false;
@@ -209,6 +202,7 @@ if (container) {
 (container = document.createElement("DIV")).id = containerId;
 container.style.cssText = shrinkChildStyle;
 var iframe = document.createElement("IFRAME");
+iframe.id = "wai-headers-check";
 iframe.style.width = "100%";
 iframe.style.height = "100%";
 iframe.style.borderWidth = "0";
@@ -238,7 +232,7 @@ iframe.onload = function() {
   iframe.onload = function() {
   };
   (doc = iframe.contentWindow.document).open();
-  doc.write('<html> <head> <meta name="viewport" content="width=device-width,minimum-scale=1.0,initial-scale=1,user-scalable=yes"><style>*{margin:0;padding:0;border:0}body{font:14px/1.6 sans;font-family:Noto Sans,Trebuchet MS,Helvetica Neue,Arial,sans-serif;color:#1d1d1d;background:rgba(255,255,255,0.95);}main{white-space:nowrap;overflow-x:hidden;text-overflow: ellipsis}ul{margin:0 0 0 -10px;padding:0}li{color:#1d1d1d;list-style:none;margin-left:20px;display:-webkit-box;display:-ms-flexbox;display:flex}main a{color:inherit;text-decoration:none;display:inline-block;max-width:30em;overflow:hidden;text-overflow:ellipsis;padding:2px 4px 2px 25px;position:relative;line-height: 1.3;border-radius:3px}main a>.level{display:inline-block;background-color:#003366;color:#FAFAFC;font-size:85%;font-weight:bold;width:2.7ex;height:2.7ex;text-align:center;-webkit-box-sizing:border-box;box-sizing:border-box;position:absolute;left:2px;top:2px;padding-top:1px}li.hidden{color:black;text-decoration:line-through;opacity:0.5}.result:not(.show-hidden) li.hidden{display:none}.result.mark-visuallyhidden li.visuallyhidden .level:before{color:inherit}.result.mark-visuallyhidden li.visuallyhidden .level{border:1px dashed;background:white}.result.mark-visuallyhidden li.visuallyhidden:after{content:"(visually hidden)"}li.wrong-level{color:#AF3A37}a.is-active{-webkit-box-shadow:0 0 5px 1px #3CBEFF;box-shadow:0 0 5px 1px #3CBEFF}main a:hover{background-color:#003366}main a:hover>.text{color:#FAFAFC}main a:hover>.level{background-color:transparent}fieldset,main{padding: 5px 15px 15px 15px;margin-bottom:1em;border-bottom:1px solid #eee}footer{padding: 0 15px 15px 15px}legend{margin-right:1em;font-weight:bold}@media(min-width:340px){legend{float:left}}.options{display:-webkit-box;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap}.options .input{margin-right:2em}.count{background:#ddd;border-radius:4px;padding:1px 3px}.tooltip{font-family:Trebuchet MS,Helvetica Neue,Arial,sans-serif;position:relative}.tooltip:before{content:\'\u2139\ufe0f\'}.tooltip:focus:after,.tooltip:hover:after{content:attr(title);position:absolute;background:white;padding:4px 10px;top:70%;left:70%;width:10em;-webkit-box-shadow:0 2px 20px rgba(0,0,0,0.3);box-shadow:0 2px 20px rgba(0,0,0,0.3);white-space:normal;border-radius:8px;border-top-left-radius:0;z-index:1;pointer-events:none}.button-close{position:absolute;top:15px;right:15px;padding:5px 10px;border-radius:5px;border:1px solid transparent;font-size:inherit;color:white;background-color:#003366;cursor:pointer}.button-close:hover{color:white;background-color:#003366}#wai-info-box header{font-weight:700;background-color:#f2f2f2;color:#005a6a;padding:8px 16px;}#wai-info-box header a{float:right;text-decoration:none}</style></head><body><aside id="wai-info-box"><header>Page headings<a href="" data-action="close" aria-label=\'dismiss\'>X</a></header><fieldset><legend>Options</legend><div class="options"><div class="input"><input type="checkbox" name="options" id="o-hidden"> <label for="o-hidden">Show hidden <span class="count" id="o-hidden-count"></span> <span class="tooltip" title="also hidden for screenreaders" tabindex="0"></span></label> </div> <div class="input"> <input type="checkbox" name="options" id="o-visuallyhidden"> <label for="o-visuallyhidden">Mark visually hidden <span class="count" id="o-visuallyhidden-count"></span> <span class="tooltip" title="only visible for screenreaders" tabindex="0"></span></label> </div> <div class="input"> <input type="checkbox" name="options" id="o-highlight"> <label for="o-highlight">Hover-Highlight <span class="tooltip" title="Highlight the corresponding heading when hovering over elements of the page" tabindex="0"></span></label></div></fieldset></div><main id="result" class="result"></main><footer>Find out more about <a href="/wai/easy-checks/headings/">Checking Headings</a></footer></body></html>');
+  doc.write('<html> <head> <meta name="viewport" content="width=device-width,minimum-scale=1.0,initial-scale=1,user-scalable=yes"><style>*{margin:0;padding:0;border:0}body{font:14px/1.6 sans;font-family:Noto Sans,Trebuchet MS,Helvetica Neue,Arial,sans-serif;color:#1d1d1d;background:rgba(255,255,255,0.95);}main{white-space:nowrap;overflow-x:hidden;text-overflow: ellipsis}ul{margin:0 0 0 -10px;padding:0}li{color:#1d1d1d;list-style:none;margin-left:20px;display:-webkit-box;display:-ms-flexbox;display:flex}main a{color:inherit;text-decoration:none;display:inline-block;max-width:30em;overflow:hidden;text-overflow:ellipsis;padding:2px 4px 2px 25px;position:relative;line-height: 1.3;border-radius:3px}main a>.level{display:inline-block;background-color:#003366;color:#FAFAFC;font-size:85%;font-weight:bold;width:2.7ex;height:2.7ex;text-align:center;-webkit-box-sizing:border-box;box-sizing:border-box;position:absolute;left:2px;top:2px;padding-top:1px}li.hidden{color:black;text-decoration:line-through;opacity:0.5}.result:not(.show-hidden) li.hidden{display:none}.result.mark-visuallyhidden li.visuallyhidden .level:before{color:inherit}.result.mark-visuallyhidden li.visuallyhidden .level{border:1px dashed;background:white}.result.mark-visuallyhidden li.visuallyhidden:after{content:"(visually hidden)"}li.wrong-level{color:#AF3A37}a.is-active{-webkit-box-shadow:0 0 5px 1px #3CBEFF;box-shadow:0 0 5px 1px #3CBEFF}main a:hover{background-color:#003366}main a:hover>.text{color:#FAFAFC}main a:hover>.level{background-color:transparent}fieldset,main{padding: 5px 15px 15px 15px;margin-bottom:1em;border-bottom:1px solid #eee}footer{padding: 0 15px 15px 15px}legend{margin-right:1em;font-weight:bold}@media(min-width:340px){legend{float:left}}.options{display:-webkit-box;display:-ms-flexbox;display:flex;-ms-flex-wrap:wrap;flex-wrap:wrap}.options .input{margin-right:2em}.count{background:#ddd;border-radius:4px;padding:1px 3px}.tooltip{font-family:Trebuchet MS,Helvetica Neue,Arial,sans-serif;position:relative}.tooltip:before{content:\'\u2139\ufe0f\'}.tooltip:focus:after,.tooltip:hover:after{content:attr(title);position:absolute;background:white;padding:4px 10px;top:70%;left:70%;width:10em;-webkit-box-shadow:0 2px 20px rgba(0,0,0,0.3);box-shadow:0 2px 20px rgba(0,0,0,0.3);white-space:normal;border-radius:8px;border-top-left-radius:0;z-index:1;pointer-events:none}.button-close{position:absolute;top:15px;right:15px;padding:5px 10px;border-radius:5px;border:1px solid transparent;font-size:inherit;color:white;background-color:#003366;cursor:pointer}.button-close:hover{color:white;background-color:#003366}#wai-info-box header{font-weight:700;background-color:#f2f2f2;color:#005a6a;padding:8px 16px;}#wai-info-box header a{float:right;text-decoration:none}</style></head><body><aside id="wai-info-box" tabindex="-1"><header>Page headings<a href="" data-action="close" aria-label=\'dismiss\'>X</a></header><fieldset><legend>Options</legend><div class="options"><div class="input"><input type="checkbox" name="options" id="o-hidden"> <label for="o-hidden">Show hidden <span class="count" id="o-hidden-count"></span> <span class="tooltip" title="also hidden for screenreaders" tabindex="0"></span></label> </div> <div class="input"> <input type="checkbox" name="options" id="o-visuallyhidden"> <label for="o-visuallyhidden">Mark visually hidden <span class="count" id="o-visuallyhidden-count"></span> <span class="tooltip" title="only visible for screenreaders" tabindex="0"></span></label> </div> <div class="input"> <input type="checkbox" name="options" id="o-highlight"> <label for="o-highlight">Hover-Highlight <span class="tooltip" title="Highlight the corresponding heading when hovering over elements of the page" tabindex="0"></span></label></div></fieldset></div><main id="result" class="result"></main><footer>Find out more about <a href="/wai/easy-checks/headings/">Checking Headings</a></footer></body></html>');
   doc.close();
   var openLoginScreenBtn = doc.querySelector('[data-action="close"]');
   if (openLoginScreenBtn) {
@@ -266,7 +260,6 @@ iframe.onload = function() {
   register(doc.getElementById("o-highlight"));
   load();
   doc.addEventListener("click", function(e) {
-		console.log("function: unnamed mouseover thing at the end");
     var t;
     if ("A" === e.target.nodeName.toUpperCase() ? t = e.target : e.target.parentElement && "A" === e.target.parentElement.nodeName.toUpperCase() && (t = e.target.parentElement), t) {
       var j = parseInt(t.getAttribute("href").substr(1), 10);
@@ -276,3 +269,5 @@ iframe.onload = function() {
   window.addEventListener("resize", load);
 };
 document.body.appendChild(container);
+iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+iframeDoc.getElementById("wai-info-box").focus();
